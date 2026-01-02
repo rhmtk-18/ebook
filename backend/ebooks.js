95ca81d184dd6602af01ce021dc0997c43868be3
rhmtk-18 <rhmt6175@gmail.com>
2026-01-02 13:16:23 +0530

    change local path

--- backend/ebooks.js
+++ backend/ebooks.js
@@ -7,7 +7,7 @@ let app = express()
 
 app.use(cors(
     {
-        origin :  ["https://ebook-liard-one.vercel.app/" , "http://localhost:5173/" ] 
+        origin :  ["https://ebook-liard-one.vercel.app" , "http://localhost:5173" ] 
     }
 ))
 app.use(express.json())
